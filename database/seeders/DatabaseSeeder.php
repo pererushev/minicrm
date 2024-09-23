9acc71508c04efa132c5adf731f6bb332395bf76
Roman.Pererushev <roman.pererushev@gmail.com>
2024-09-23 20:30:00 +0300

    Роли - админ юзер, spatie permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\RoleEnum;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(RoleSeeder::class);
+
         User::factory(10)->create();
+
+        User::factory()->create([
+            'first_name' => 'Admin',
+            'last_name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => 'secret'
+        ])->syncRoles([RoleEnum::ADMIN]);
     }
 }
